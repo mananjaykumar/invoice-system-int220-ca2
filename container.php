56bd118e2bdfa64a36d7f3c7352dabd038c0d0cd
Mananjay <sinharishab100@gmail.com>
2022-04-04 13:55:22 +0530

    some modifications done

--- container.php
+++ container.php
@@ -20,3 +20,4 @@
 	<div class="container">
     <div class="card">
       <div class="card-body">
+        
--- index.php
+++ index.php
@@ -1,13 +1,27 @@
 <?php 
+session_start();
 include('header.php');
 $loginError = '';
+if (!empty($_POST['email']) && !empty($_POST['pwd'])) {
+	include 'Invoice.php';
+	$invoice = new Invoice();
+	$user = $invoice->loginUsers($_POST['email'], $_POST['pwd']); 
+	if(!empty($user)) {
+		$_SESSION['user'] = $user[0]['first_name']."".$user[0]['last_name'];
+		$_SESSION['userid'] = $user[0]['id'];
+		$_SESSION['email'] = $user[0]['email'];		
+		$_SESSION['address'] = $user[0]['address'];
+		$_SESSION['mobile'] = $user[0]['mobile'];
+		header("Location:invoice_list.php");
+	} else {
+		$loginError = "Invalid email or password!";
+	}
+}
 ?>
 <title>Invoice System</title>
 <script src="js/invoice.js"></script>
 <link href="css/style.css" rel="stylesheet">
-<?php 
-include('container.php');
-?>
+<?php include('container.php');?>
 <style type="text/css">
 	.form-control {
     height: 46px;
@@ -19,7 +33,6 @@ include('container.php');
     background: rgb(213, 211, 222);
 }
 </style>
-
 <div class="row" style="bbackground: #0F2027;  /* fallback for old browsers */
 background: -webkit-linear-gradient(to right, #2C5364, #203A43, #0F2027);  /* Chrome 10-25, Safari 5.1-6 */
 background: linear-gradient(to right, #2C5364, #203A43, #0F2027); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
@@ -50,4 +63,4 @@ background: linear-gradient(to right, #2C5364, #203A43, #0F2027); /* W3C, IE 10+
 	</div>		
 </div>		
 </div>
-<?php include('footer.php'); ?>
+<?php include('footer.php');?>
